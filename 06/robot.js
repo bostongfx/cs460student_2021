ce9c3c7bd2456e49be8a7b1b475588ac1de04f6d
jpb93 <jeremy.burkhardt@gmail.com>
2021-10-31 15:34:40 -0400

    Add rest of limbs

--- 06/robot.js
+++ 06/robot.js
@@ -19,6 +19,9 @@ Robot = function(x, y, z) {
   this.torso.position.y = -30
 
   this.neck.add(this.torso)
+  
+  // LEFT ARM
+  // ===========================================
 
   this.left_upper_arm = new THREE.Bone()
   this.left_upper_arm.position.y = -5
@@ -37,6 +40,69 @@ Robot = function(x, y, z) {
   this.left_hand.position.x = -1
 
   this.left_lower_arm.add(this.left_hand)
+
+  // RIGHT ARM
+  //================================================
+
+  this.right_upper_arm = new THREE.Bone()
+  this.right_upper_arm.position.y = -5
+  this.right_upper_arm.position.x = -5
+
+  this.neck.add(this.right_upper_arm)
+
+  this.right_lower_arm = new THREE.Bone()
+  this.right_lower_arm.position.y = -15
+  this.right_lower_arm.position.x = -10
+
+  this.right_upper_arm.add(this.right_lower_arm)
+
+  this.right_hand = new THREE.Bone()
+  this.right_hand.position.y = -5
+  this.right_hand.position.x = 1
+
+  this.right_lower_arm.add(this.right_hand)
+
+  // LEFT LEG
+  // ====================================================
+
+  this.left_upper_leg = new THREE.Bone()
+  this.left_upper_leg.position.y = -5
+  this.left_upper_leg.position.x = 5
+
+  this.torso.add(this.left_upper_leg)
+
+  this.left_lower_leg = new THREE.Bone()
+  this.left_lower_leg.position.y = -15
+  this.left_lower_leg.position.x = 10
+
+  this.left_upper_leg.add(this.left_lower_leg)
+
+  this.left_foot = new THREE.Bone()
+  this.left_foot.position.y = -5
+  this.left_foot.position.x = 1
+
+  this.left_lower_leg.add(this.left_foot)
+
+  // RIGHT LEG
+  // ====================================================
+
+  this.right_upper_leg = new THREE.Bone()
+  this.right_upper_leg.position.y = -5
+  this.right_upper_leg.position.x = -5
+
+  this.torso.add(this.right_upper_leg)
+
+  this.right_lower_leg = new THREE.Bone()
+  this.right_lower_leg.position.y = -15
+  this.right_lower_leg.position.x = -10
+
+  this.right_upper_leg.add(this.right_lower_leg)
+
+  this.right_foot = new THREE.Bone()
+  this.right_foot.position.y = -5
+  this.right_foot.position.x = -1
+
+  this.right_lower_leg.add(this.right_foot)
 
 
 
